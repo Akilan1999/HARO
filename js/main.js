8ef31fe22dc078b6180a508bbe9a8599f9a94cda
Akilan1999 <akilanselva@hotmail.com>
2018-07-14 21:07:42 +0400

    nav fix

--- js/main.js
+++ js/main.js
@@ -1,42 +1,9 @@
-
-var modal = document.getElementById('nav_modal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("hamburger");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-
-// nav bar color change 
-var top1 = $('#header').offset().top;
-var top2 = $('#aboutus').offset().top;
-var top3 = $('#contact').offset().top;
-
-$(document).scroll(function() {
-  var scrollPos = $(document).scrollTop();
-  if (scrollPos >= top1 && scrollPos < top2) {
-    $('#nav').css('background', 'white');
-  } else if (scrollPos >= top2 && scrollPos < top3) {
-    $('#nav').css('background', 'white');
-  } else if (scrollPos >= top3) {
-    $('#nav').css('background', 'white');
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "nav") {
+      x.className += " responsive";
+  } else {
+      x.className = "nav";
   }
-});
+}
+
